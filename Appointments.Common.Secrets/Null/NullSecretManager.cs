da3fa9db1770af34cbf089e64c8a7d6cfea972ce
viex <carp_97@outlook.com>
2023-07-20 11:01:40 -0600

    Improved DI for secrets.

---
+++ Appointments.Common.Secrets/Null/NullSecretManager.cs
@@ -0,0 +1,21 @@
+﻿using Appointments.Common.Secrets.Exceptions;
+
+namespace Appointments.Common.Secrets.Null;
+
+internal class NullSecretManager : ISecretManager
+{
+    public Task DeleteAsync(string key)
+    {
+        throw new NullSecretManagerException();
+    }
+
+    public Task<string> GetAsync(string key)
+    {
+        throw new NullSecretManagerException();
+    }
+
+    public Task SetAsync(string key, string value)
+    {
+        throw new NullSecretManagerException();
+    }
+}
--- Appointments.Common.Secrets/Redis/DependencyInjection/RedisSecretManagerExtensions.cs
+++ Appointments.Common.Secrets/Redis/DependencyInjection/RedisSecretManagerExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using Appointments.Common.Secrets.Crypto;
+using Appointments.Common.Secrets.Null;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace Appointments.Common.Secrets.Redis.DependencyInjection;
 
@@ -7,9 +10,47 @@ public static class RedisSecretManagerExtensions
 {
     public static IServiceCollection AddRedisSecretManager(this IServiceCollection services, IRedisSecretManagerOptions options)
     {
-        var redisDatabase = new RedisDatabaseFactory(options.ConnectionString).CreateDatabase();
+        var logger = LoggerFactory
+            .Create(builder => builder
+                .AddConsole())
+            .CreateLogger(nameof(RedisSecretManagerExtensions));
+
+        var database = GetDatabaseOrDefault(
+            logger,
+            options.ConnectionString);
+
+        if (database is null)
+        {
+            logger.LogWarning("The {ServiceName} will be used as a fallback.", nameof(NullSecretManager));
+            return services.AddNullSecretManager();
+        }
+
         var cryptoService = new CryptoService(new CryptoOptions(options.Key));
-        var secretManager = new RedisSecretManager(redisDatabase, cryptoService);
+        var secretManager = new RedisSecretManager(database, cryptoService);
         return services.AddSingleton<ISecretManager>(secretManager);
+    }
+
+    private static IDatabase? GetDatabaseOrDefault(ILogger logger, string connectionString)
+    {
+        logger.LogInformation("Connecting to Redis...");
+
+        try
+        {
+            var connectionMultiplexer = ConnectionMultiplexer.Connect(connectionString, config =>
+            {
+                //config.AbortOnConnectFail = true;
+                config.ConnectRetry = 2;
+                config.IncludeDetailInExceptions = true;
+            });
+
+            logger.LogInformation("A successful connection to Redis was established!");
+
+            return connectionMultiplexer.GetDatabase();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Could not connect to Redis.");
+            return null;
+        }
     }
 }
