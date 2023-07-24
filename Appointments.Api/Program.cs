2ab0a7c3cb2cd00d02d5e648e744b5f0696f1936
viex <carp_97@outlook.com>
2023-07-24 09:43:06 -0600

    Added CORS.

--- Appointments.Api/Program.cs
+++ Appointments.Api/Program.cs
@@ -3,11 +3,30 @@ using Appointments.Api.Filters.Exceptions.ProblemDetailsFactories;
 using Appointments.Application.DependencyInjection;
 using Appointments.Infrastructure.DependencyInjection;
 
+const string DefaultCorsPolicy = "DefaultCorsPolicy";
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(config =>
 {
     config.Filters.Add(typeof(ExceptionFilter));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(DefaultCorsPolicy, policy =>
+    {
+        var whitelist = builder.Configuration
+            .GetRequiredSection("Cors")
+            .GetRequiredSection("Default")
+            .Get<string[]>()
+            ?? Array.Empty<string>();
+
+        policy
+            .WithOrigins(whitelist)
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
 });
 
 builder.Services.AddEndpointsApiExplorer();
@@ -28,6 +47,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(DefaultCorsPolicy);
 
 app.UseAuthorization();
 
