31745f83d67dc4f95a043771d4dc2ee39dca54c5
viex <carp_97@outlook.com>
2023-07-30 18:13:40 -0600

    feat(frontend): add bootstrap

---
+++ Appointments.Web.Admin/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import 'bootstrap/dist/css/bootstrap.min.css';
+	import 'bootstrap-icons/font/bootstrap-icons.min.css';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		await import('bootstrap'); // Import on the browser
+	});
+</script>
+
+<slot />
