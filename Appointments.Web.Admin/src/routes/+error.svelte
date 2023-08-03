c4787002ba9b50d346c1d438ef3c772da343550d
viex <carp_97@outlook.com>
2023-08-03 11:20:11 -0600

    feat(frontend): add server side error page

---
+++ Appointments.Web.Admin/src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/stores';
+	import { ErrorPage, message, statusCode } from '$lib/error-page';
+
+	message.set($page.error?.message ?? 'An unexpected error occurred');
+	statusCode.set($page.status);
+</script>
+
+<div class="container py-5">
+	<ErrorPage />
+</div>
